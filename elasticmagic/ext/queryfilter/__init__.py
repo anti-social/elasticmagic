f64ec4864dd000e4095c2e6a1a0a5cc2f94f4dc2
Alexander Koval <kovalidis@gmail.com>
2019-01-08 15:14:08 +0200

    Removed GroupedPageFilter

--- elasticmagic/ext/queryfilter/__init__.py
+++ elasticmagic/ext/queryfilter/__init__.py
@@ -2,6 +2,6 @@ from .queryfilter import QueryFilter, FacetFilter, RangeFilter
 from .queryfilter import FacetQueryFilter, FacetQueryValue
 from .queryfilter import SimpleFilter, SimpleQueryFilter, SimpleQueryValue
 from .queryfilter import OrderingFilter, OrderingValue
-from .queryfilter import GroupedPageFilter, PageFilter
+from .queryfilter import PageFilter
 from .queryfilter import NestedFacetFilter, NestedRangeFilter
 from .queryfilter import BinaryFilter
